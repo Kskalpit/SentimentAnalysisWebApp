57481654e932451c3179e6e798309c92e925feb5
Kskalpit <kskalpit09@gmail.com>
2025-05-05 23:53:38 -0400

    Ready

--- SentimentAnalysis/sentiment_analysis.py
+++ SentimentAnalysis/sentiment_analysis.py
@@ -7,15 +7,10 @@ def sentiment_analyser (text_to_analyse):
     header ={"grpc-metadata-mm-model-id": "sentiment_aggregated-bert-workflow_lang_multi_stock"}
     myobj = { "raw_document": { "text": text_to_analyse } }
     response = requests.post(url, json = myobj, headers=header)
-    return response.text
 
-
-    response = sentiment_analyser("I love this new technology")
-
-    formatted_response = json.loads(response)
+    formatted_response = json.loads(response.text)
 
     label = formatted_response['documentSentiment']['label']
     score = formatted_response['documentSentiment']['score']
-
     return {'label': label, 'score': score}
 
